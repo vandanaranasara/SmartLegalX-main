4fff4da099848bed6fb082fe27751d844aa90eb3
vandanaranasara <vandanaranasara03102004@gmail.com>
2025-05-30 18:38:16 +0530

    Add files via upload

---
+++ Model/app.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS from flask_cors
+import asyncio
+
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.prompts import PromptTemplate
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for your app
+
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+def set_custom_prompt():
+    prompt = PromptTemplate(template=custom_prompt_template,
+                            input_variables=['context', 'question'])
+    return prompt
+
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
+
+def load_llm():
+    llm = CTransformers(
+        model="TheBloke/Llama-2-7B-Chat-GGML",
+        model_type="llama",
+        max_new_tokens=512,
+        temperature=0.5
+    )
+    return llm
+
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                       model_kwargs={'device': 'cpu'})
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+    return qa
+
+async def answer_question(question):
+    chain = qa_bot()
+    res = await chain.acall(question)
+    answer = res["result"]
+    sources = res["source_documents"]
+    if sources:
+        answer += f"\nSources:" + str(sources)
+    else:
+        answer += "\nNo sources found"
+    return answer
+
+@app.route('/ask', methods=['POST'])
+async def ask_question():
+    data = request.get_json()
+    print(data)
+    query = data.get('query')
+    answer = await answer_question(query)  # Corrected function name
+    return jsonify({"answer": answer})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 5001)
---
+++ Model/chainlit.md
@@ -0,0 +1,5 @@
+# Welcome to SmartLegalX! 
+
+Hi there, 👋 We're excited to have you on board. This is a powerful bot designed to help you ask queries related to your data/knowledge.
+Happy chatting! 💻😊
+
